52203875c048c2bb001fd7eaf3ea6e5f8c94b2b0
David Buckley <runalldayandnight@gmail.com>
2022-04-21 10:00:32 -0600

    Book pages work

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext, FormEvent } from "react"
+import { useEffect, useState, useContext, FormEvent } from "react";
 import {
   FormControl,
   Button,
@@ -16,7 +16,7 @@ import {
   Flex,
   Stack,
 } from "@chakra-ui/react";
-import { AuthContext, AuthContextType } from "../../providers"
+import { AuthContext, AuthContextType } from "../../providers";
 
 import Image from "next/image";
 import { Checkout } from "types/library";
@@ -24,26 +24,24 @@ import { Checkout } from "types/library";
 export default function Books() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [checkouts, setCheckouts] = useState<Array<Checkout>>();
-  const { auth } = useContext(AuthContext) as AuthContextType
-
-  
+  const { auth } = useContext(AuthContext) as AuthContextType;
 
   useEffect(() => {
     async function getCheckouts() {
       const response = await fetch(process.env.API_URL + "library/checkouts/", {
         headers: {
-          "Authorization": `Token ${auth.user.token}`
-        }
-      })
-      const checkouts = await response.json()
-      console.log(checkouts)
+          Authorization: `Token ${auth.user.token}`,
+        },
+      });
+      const checkouts = await response.json();
+      console.log(checkouts);
       if (checkouts instanceof Array) {
-        setCheckouts(checkouts as Array<Checkout>)
+        setCheckouts(checkouts as Array<Checkout>);
       }
     }
 
-    getCheckouts()
-  }, [auth])
+    getCheckouts();
+  }, [auth]);
 
   return (
     <Box mb={8} w="full">
@@ -57,19 +55,17 @@ export default function Books() {
             const checkout = val as Checkout;
             return (
               <Flex direction="row" margin={5}>
-                <Link href={`/checkouts/${checkout.isbn}`}>{checkout.image ? <img src={checkout.image} height={100} /> : <img src="/Book_Placeholder.png" height={100} />}</Link>
+                {/* <Link href={`/checkouts/${checkout.isbn}`}>{checkout.image ? <img src={checkout.image} height={100} /> : <img src="/Book_Placeholder.png" height={100} />}</Link>
                 <Box margin={5}>
                   <Text fontSize={20}><Link href={`/checkouts/${checkout.isbn}`}>{checkout.title}</Link></Text>
                   <Text fontSize={15}>{checkout.last_name}, {checkout.first_name}</Text>
                   <Text fontSize={13}>Call Number: {checkout.call_number}</Text>
-                </Box>
+                </Box> */}
               </Flex>
-            )
+            );
           })}
-
         </Box>
       </Flex>{" "}
-
     </Box>
   );
 }
